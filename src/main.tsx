a94f8ac90d036a6f79c4db2336b2febeaa19be84
ncollinsmkl <ncollins@merkleinc.com>
2026-09-15 12:01:12 +0100

    Lots of changes to convert HTML calculator files to TSX files

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/global.css";
+import "../../styles/calculators.css";
 import Home from "./pages/Home";
 import AuditMocks from "./pages/AuditMocks";
 import Privacy from "./pages/Privacy";
 import ScrollToTop from "./components/ScrollToTop";
 
-// Import the new calculator pages
 import CalculatorsIndex from "./pages/calculators/Index";
 import AIValueCalculator from "./pages/calculators/AIValueCalculator";
 import DataStrengthCalculator from "./pages/calculators/DataStrengthCalculator";
@@ -15,15 +15,12 @@ import DataStrengthCalculator from "./pages/calculators/DataStrengthCalculator";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      {/* Reset scroll position on every route change. */}
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy" element={<Privacy />} />
-        {/* Comparison page for the user to pick a questionnaire design.
-            Remove once a design is chosen. */}
         <Route path="/mocks/audit" element={<AuditMocks />} />
-        
+
         {/* Calculator Routes */}
         <Route path="/calculators" element={<CalculatorsIndex />} />
         <Route path="/calculators/ai-value-calculator" element={<AIValueCalculator />} />
