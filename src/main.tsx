25c6770a708072d61ac559275cb55837f4ac367e
ncollinsmkl <ncollins@merkleinc.com>
2026-09-15 12:03:26 +0100

    Updated Main.tsx with change the CSS link

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/global.css";
-import "../../styles/calculators.css";
+import "./styles/calculators.css";
 import Home from "./pages/Home";
 import AuditMocks from "./pages/AuditMocks";
 import Privacy from "./pages/Privacy";
